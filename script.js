043e53cc3dba8403521a5b816f76d2428ee2a4ca
raeganfaith <57030395+raeganfaith@users.noreply.github.com>
2022-08-26 16:11:13 +0800

    Added contents to new pages

---
+++ script.js
@@ -0,0 +1,83 @@
+//String Concatenation
+function myQuestion1() {
+  var stringVar = "What is your Full Name? ";
+  alert(stringVar + "Raegan Faith F. Paguirigan");
+}
+function myQuestion2() {
+  var stringVar = "What is your favorite color? ";
+  alert(stringVar + "Pink");
+}
+function myQuestion3() {
+  var stringVar = "Who is your favorite artist? ";
+  alert(stringVar + "Taylor Momsen");
+}
+function myQuestion4() {
+  var stringVar = "What is your favorite pet? ";
+  alert(stringVar + "Cat");
+}
+function myQuestion5() {
+  var stringVar = "Where is your goal destination?  ";
+  alert(stringVar + "Tokyo, Japan");
+}
+function myQuestion6() {
+  var stringVar = "What is your zodiac sign? ";
+  alert(stringVar + "Cancer");
+}
+function myQuestion7() {
+  var stringVar = "When is your birthday? ";
+  alert(stringVar + "July 10");
+}
+function myQuestion8() {
+  var stringVar = "What is your comfort food? ";
+  alert(stringVar + "Ice Cream");
+}
+function myQuestion9() {
+  var stringVar = "What is your pet peeve? ";
+  alert(stringVar + "Loud or noisy people in the cafe.");
+}
+function myQuestion10() {
+  var stringVar = "What is your favorite band? ";
+  alert(stringVar + "Twenty One Pilots");
+}
+
+//Basic Math
+function myMath1() {
+  var numberVar = 1;
+  alert(numberVar + 3);
+}
+function myMath2() {
+  var numberVar = 10;
+  alert(numberVar * 2);
+}
+function myMath3() {
+  var numberVar = 15;
+  alert(numberVar + 2);
+}
+function myMath4() {
+  var numberVar = 5;
+  alert(numberVar + 0);
+}
+function myMath5() {
+  var numberVar = 8;
+  alert(numberVar + 2);
+}
+function myMath6() {
+  var numberVar = 1;
+  alert(numberVar + 6);
+}
+function myMath7() {
+  var numberVar = 3;
+  alert(numberVar + 5);
+}
+function myMath8() {
+  var numberVar = 5;
+  alert(numberVar - 2);
+}
+function myMath9() {
+  var numberVar = 10;
+  alert(numberVar / 5);
+}
+function myMath10() {
+  var numberVar = 1;
+  alert(numberVar * 9);
+}
